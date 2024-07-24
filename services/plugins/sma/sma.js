e1c902166072cfa8e735192a88ebb4c375a52dee
faigyTo <f4167460@gmail.com>
2024-07-24 20:39:17 +0300

    24/07 change things with zilberman

--- services/plugins/sma/sma.js
+++ services/plugins/sma/sma.js
@@ -1,31 +1,40 @@
 import smaParamsJson from './sma.json' assert {'type': 'json'};
 
 export default class sma {
+
+	valueFunc = undefined;
+
 	calcSma(data, length, func) {
 		let dataEndSliceData = data.slice(-length);
 		return dataEndSliceData.reduce((acc, elem) => acc + func(elem) / length, 0)
 	}
+
+	checkNotificationByUpOrDown(a,b){
+		return (a>b&&(a-b)/a>smaParamsJson.minimalDiffPrecents);
+	}
+
 	execute(data, params) {
 		// {length,upOrDown,symbol,func}
+
 		let { length, upOrDown, func } = params;
 		console.log(`--------------- sma_${upOrDown}_${length} --------------------`);
-		let funcConverted = eval('(' + func + ')');
+		if (!this.valueFunc)
+			this.valueFunc = eval('(' + func + ')');
 		let todayCandle = data[length - 1];
-		let todaySma = this.calcSma(data, length, funcConverted);
-		let todayValue = funcConverted(data[data.length - 1]);
+		let todaySma = this.calcSma(data, length, this.valueFunc);
+		let todayValue = this.valueFunc(data[data.length - 1]);
+		let obj = {
+			up: (todayValue, todaySma, todayCandle) => {
+				return this.checkNotificationByUpOrDown(todayValue,todaySma)?{ type: `sma_${upOrDown}_${length}`, candle: todayCandle }
+				: { type: "sma", candle: undefined };
+			}, down: (todayValue, todaySma, todayCandle) => {
+				if (todaySma > todayValue && (todaySma - todayValue) / todaySma > smaParamsJson.minimalDiffPrecents)
+					return { type: `sma_${upOrDown}_${length}`, candle: todayCandle };
+				return { type: "sma", candle: undefined };
+			}
+		}
 		console.log("today sma->", todaySma, "today value->", todayValue);
-		if (upOrDown == "up" && todayValue > todaySma && (todayValue - todaySma) / todayValue > smaParamsJson.minimalDiffPrecents) {
-			console.log("find", { type: `sma_${upOrDown}_${length}`, value: todayValue, volume: todayCandle.Volume });
-			return { type: `sma_${upOrDown}_${length}`, value: todayValue, volume: todayCandle.Volume };
-		}
-		else if (upOrDown == "down" && todaySma > todayValue && (todaySma - todayValue) / todaySma > smaParamsJson.minimalDiffPrecents) {
-			console.log("find", { type: `sma_${upOrDown}_${length}`, value: todayValue, volume: todayCandle.Volume });
-			return { type: `sma_${upOrDown}_${length}`, value: todayValue, volume: todayCandle.Volume };
-		}
-		else {
-			console.log("not find", { type: undefined });
-			return { type: undefined };
-		}
+		return obj[upOrDown](todayValue, todaySma, todayCandle);
 	}
 }
 
